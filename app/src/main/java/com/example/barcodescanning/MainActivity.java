227b8fb46f1e58ba485a60af692e5d42c38106b6
PranavPMehta <46414741+PranavPMehta@users.noreply.github.com>
2025-01-15 19:01:51 +0530

    Have checks of duplicate barcodes and correct calculation for char start and numeric start (#1)

---
+++ app/src/main/java/com/example/barcodescanning/MainActivity.java
@@ -0,0 +1,192 @@
+package com.example.barcodescanning;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+import static com.example.barcodescanning.ScannedDetails.netBarcodeNos;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private SurfaceView surfaceView;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+    private ToneGenerator toneGen1;
+    private TextView barcodeText;
+    private String barcodeData;
+    private static final String PREFS_NAME = "MyPrefsFile";
+    private static final String RADIO_BUTTON_STATE = "radioButtonState";
+    public static RadioButton allowDuplicates,disAllowDuplicates;
+    private RadioGroup radioGroup;
+
+    private Button btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, ScannedDetails.class);
+
+        toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+        surfaceView = findViewById(R.id.surface_view);
+        barcodeText = findViewById(R.id.barcode_text);
+        btnNext=findViewById(R.id.btnNext);
+        radioGroup=findViewById(R.id.radioGroup);
+        allowDuplicates=findViewById(R.id.allowDuplicate);
+        disAllowDuplicates=findViewById(R.id.disAllowDuplicate);
+        initialiseDetectorsAndSources();
+
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        int savedState = preferences.getInt(RADIO_BUTTON_STATE, R.id.allowDuplicate);
+
+        // Set the previously saved radio button to be selected
+        radioGroup.check(savedState);
+
+        // Set an OnCheckedChangeListener to save the selected state
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            // Save the selected radio button's ID to SharedPreferences
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt(RADIO_BUTTON_STATE, checkedId);
+            editor.apply();
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (barcodeText.getText().toString().equals("Barcode Text")) {
+                    Toast.makeText(MainActivity.this, "Please scan the barcode and then click next", Toast.LENGTH_SHORT).show();
+                } else {
+                    intent.putExtra("barcodeNetWeight",barcodeText.getText().toString());
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+    }
+
+    private void initialiseDetectorsAndSources() {
+
+        //Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true) //you should add this feature
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(surfaceView.getHolder());
+                    } else {
+                        ActivityCompat.requestPermissions(MainActivity.this, new
+                                String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CAMERA_PERMISSION);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                // Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+
+
+                    barcodeText.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+//                            System.out.println("Barcode Value " + barcodes.size());
+//                            System.out.println("Barcode Value " + barcodes.valueAt(0).displayValue);
+//                            System.out.println("Barcode Value " + barcodes.valueAt(0).rawValue);
+//                            System.out.println("Barcode Value " + barcodes.valueAt(0).contactInfo);
+                            if (barcodes.valueAt(0).email != null) {
+                                barcodeText.removeCallbacks(null);
+                                barcodeData = barcodes.valueAt(0).email.address;
+                                barcodeText.setText(barcodeData);
+                                toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
+                                btnNext.setEnabled(true);
+                            } else if (barcodes.valueAt(0).displayValue != null) {
+                                barcodeData = barcodes.valueAt(0).displayValue;
+                                barcodeText.setText(barcodeData);
+                                toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
+                                btnNext.setEnabled(true);
+                            }
+                            if(disAllowDuplicates.isChecked() && netBarcodeNos.contains(barcodeData)){
+                                Toast.makeText(getApplicationContext(), "This is duplicate barcode, Re-Scan.", Toast.LENGTH_SHORT).show();
+                                btnNext.setEnabled(false);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getSupportActionBar().hide();
+        cameraSource.release();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getSupportActionBar().hide();
+        initialiseDetectorsAndSources();
+    }
+
+}
---
+++ app/src/main/java/com/example/barcodescanning/ScannedDetails.java
@@ -0,0 +1,309 @@
+package com.example.barcodescanning;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.example.barcodescanning.MainActivity.allowDuplicates;
+import static com.example.barcodescanning.MainActivity.disAllowDuplicates;
+
+public class ScannedDetails extends AppCompatActivity {
+
+    float barcodeNetWeight,totalWeight=0;
+    public static ArrayList<Float> netWeights = new ArrayList<Float>();
+    public static ArrayList<String> netBarcodeNos = new ArrayList<String>();
+    Button nextScanBtn,clearBtn,exportBtn;
+    TextView barcodeNetWeightTxtView, totalWeightTxtView;
+
+    String barcodeNo;
+    AlertDialog.Builder builder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanned_details);
+
+        nextScanBtn=findViewById(R.id.newScan);
+        clearBtn=findViewById(R.id.clearBtn);
+        exportBtn=findViewById(R.id.exportBtn);
+        barcodeNetWeightTxtView=findViewById(R.id.barcodeNetWeight);
+        totalWeightTxtView=findViewById(R.id.totalWeightTxtView);
+
+        TableLayout tl = findViewById(R.id.table);
+
+        Intent intent = getIntent();
+        barcodeNo=intent.getStringExtra("barcodeNetWeight");
+//        barcodeNo="0S5.6-1904";
+        int len=barcodeNo.length();
+        try{
+            if(!Character.isLetter(barcodeNo.charAt(0))){
+                if(barcodeNo.charAt(len-2)=='.'){    //last 2nd character
+                    barcodeNetWeight = Float.valueOf(barcodeNo.substring(len-5,len));
+                }else if(barcodeNo.contains(".")) {
+                    barcodeNetWeight = Float.valueOf(barcodeNo.substring(len-4,len));
+                    barcodeNetWeight/=10;
+                }else{
+                    barcodeNetWeight = Float.valueOf(barcodeNo.substring(len-4,len));
+                    barcodeNetWeight/=10;
+                }
+            }else{
+                if(barcodeNo.charAt(len-2)=='.'){
+                    barcodeNetWeight = Float.valueOf(barcodeNo.substring(len-5,len));
+                }else if(barcodeNo.contains(".")) {
+                    barcodeNetWeight = Float.valueOf(barcodeNo.substring(len-4,len));
+                    barcodeNetWeight/=10;
+                }else{
+                    barcodeNetWeight=0;
+                }
+            }
+        }catch(Exception e){
+            Toast.makeText(ScannedDetails.this, "This is incorrect barcode", Toast.LENGTH_SHORT).show();
+        }
+
+        System.out.println("barcodeNetWeight "+barcodeNetWeight);
+        barcodeNetWeightTxtView.setText(String.valueOf(barcodeNetWeight));
+
+        tl.removeAllViews();
+        totalWeight=0;
+        netWeights.add(barcodeNetWeight);
+        netBarcodeNos.add(barcodeNo);
+        addHeaders();
+        addData();
+        totalWeightTxtView.setText(String.valueOf(totalWeight));
+
+        nextScanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(ScannedDetails.this, MainActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearAllValues(tl);
+            }
+        });
+        builder = new AlertDialog.Builder(this);
+        final EditText editText = new EditText(this);
+
+        if (editText.getParent() != null) {
+            ((ViewGroup) editText.getParent()).removeView(editText);
+        }
+        exportBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the AlertDialog
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(ScannedDetails.this);
+                dialogBuilder.setTitle("Enter File Name")
+                        .setMessage("Please enter some file name:")
+                        .setView(editText)  // Set the EditText as the view of the dialog
+                        .setPositiveButton("Save", (dialog, which) -> {
+                            // Get the text entered by the user
+                            String userInput = editText.getText().toString();
+                            if (!userInput.isEmpty()) {
+                                // Handle saving the input (e.g., saving it in SharedPreferences, database, etc.)
+                                Toast.makeText(ScannedDetails.this, "Saved: " + userInput, Toast.LENGTH_SHORT).show();
+                                File filePath;
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                                    filePath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), userInput+".xls");
+                                }else{
+                                    filePath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + userInput + ".xls");
+                                }
+
+                                try {
+
+                                    if (!filePath.exists()) {
+                                        HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
+//                                        HSSFSheet hssfSheet = hssfWorkbook.createSheet("MySheet");
+//
+//                                        HSSFRow hssfRow = hssfSheet.createRow(0);
+//                                        HSSFCell hssfCell = hssfRow.createCell(0);
+//
+//                                        hssfCell.setCellValue(userInput);
+                                        filePath.createNewFile();
+
+                                        FileInputStream fileInputStream = new FileInputStream(filePath);
+                                        hssfWorkbook = upsertValueInExcelSheet(hssfWorkbook);
+                                        fileInputStream.close();
+
+                                        FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+                                        hssfWorkbook.write(fileOutputStream);
+                                        editText.setText("");
+                                        Toast.makeText(ScannedDetails.this, "File Created", Toast.LENGTH_SHORT).show();
+
+                                        if (fileOutputStream != null) {
+                                            fileOutputStream.flush();
+                                            fileOutputStream.close();
+                                        }
+                                        clearAllValues(tl);
+                                    }
+
+                                    else{
+
+                                        FileInputStream fileInputStream = new FileInputStream(filePath);
+                                        HSSFWorkbook hssfWorkbook = new HSSFWorkbook(fileInputStream);
+                                        hssfWorkbook = upsertValueInExcelSheet(hssfWorkbook);
+
+                                        fileInputStream.close();
+
+                                        FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+                                        hssfWorkbook.write(fileOutputStream);
+                                        editText.setText("");
+                                        Toast.makeText(ScannedDetails.this, "File Updated", Toast.LENGTH_SHORT).show();
+                                        fileOutputStream.close();
+                                        clearAllValues(tl);
+                                    }
+
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+
+                                }
+                            } else {
+                                Toast.makeText(ScannedDetails.this, "Input cannot be empty", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss()) // Dismiss the dialog on cancel
+                        .setOnDismissListener(dialog -> {
+                            // Handle the cleanup after the dialog is dismissed
+                            if (editText.getParent() != null) {
+                                ((ViewGroup) editText.getParent()).removeView(editText); // Remove the EditText from its parent
+                            }
+                        })
+                        .create();
+
+                // Show the dialog
+                dialogBuilder.show();
+
+            }
+        });
+    }
+
+    private void clearAllValues(TableLayout tl) {
+        allowDuplicates.setChecked(false);
+        disAllowDuplicates.setChecked(false);
+        barcodeNetWeightTxtView.setText("");
+        netWeights.clear();
+        netBarcodeNos.clear();
+        tl.removeAllViews();
+        totalWeight=0;
+        totalWeightTxtView.setText(String.valueOf(totalWeight));
+    }
+
+    @NonNull
+    private HSSFWorkbook upsertValueInExcelSheet(HSSFWorkbook hssfWorkbook) throws IOException {
+        HSSFSheet hssfSheet;
+        if (hssfWorkbook.getNumberOfSheets() > 0) {
+            hssfSheet = hssfWorkbook.getSheetAt(0);
+        } else {
+            hssfSheet = hssfWorkbook.createSheet("Sheet 1");
+        }
+        HSSFRow hssfRow = hssfSheet.createRow(0);
+        hssfRow.createCell(0).setCellValue("Sr No");
+        hssfRow.createCell(1).setCellValue("Barcode");
+        hssfRow.createCell(2).setCellValue("Net Weight");
+
+        int lastRowNum = 1;
+        System.out.println("lastRowNum "+lastRowNum);
+        for(int i=0;i<netWeights.size();i++){
+            hssfRow = hssfSheet.createRow(lastRowNum+i);
+
+            // Create a cell in that row, and set its value from the ArrayList
+            HSSFCell cell0 = hssfRow.createCell(0);
+            cell0.setCellValue(lastRowNum+i);
+            HSSFCell cell1 = hssfRow.createCell(1);
+            cell1.setCellValue(String.valueOf(netBarcodeNos.get(i)));
+            HSSFCell cell2 = hssfRow.createCell(2);
+            cell2.setCellValue(String.valueOf(netWeights.get(i)));
+        }
+        return hssfWorkbook;
+    }
+
+    @NonNull
+    private TableRow.LayoutParams getLayoutParams() {
+        TableRow.LayoutParams params = new TableRow.LayoutParams(
+                TableRow.LayoutParams.MATCH_PARENT,
+                TableRow.LayoutParams.WRAP_CONTENT);
+        params.setMargins(2, 0, 0, 2);
+        return params;
+    }
+
+    @NonNull
+    private TableRow.LayoutParams getTblLayoutParams() {
+        return new TableRow.LayoutParams(
+                TableRow.LayoutParams.MATCH_PARENT,
+                TableRow.LayoutParams.MATCH_PARENT);
+    }
+
+    private TextView getTextView(int id, String title, int color, int typeface, int bgColor) {
+        TextView tv = new TextView(this);
+        tv.setId(id);
+        tv.setText(title.toUpperCase());
+        tv.setTextColor(color);
+        tv.setPadding(10, 10, 10, 10);
+        tv.setTypeface(Typeface.DEFAULT, typeface);
+        tv.setBackgroundColor(bgColor);
+        tv.setLayoutParams(getLayoutParams());
+        tv.setVisibility(View.VISIBLE);
+        return tv;
+    }
+
+    public void addHeaders() {
+        TableLayout tl = findViewById(R.id.table);
+        TableRow tr = new TableRow(this);
+        tr.setLayoutParams(getLayoutParams());
+        tr.addView(getTextView(0, "Sr No", Color.WHITE, Typeface.BOLD, Color.BLUE));
+        tr.addView(getTextView(0, "Barcode", Color.WHITE, Typeface.BOLD, Color.BLUE));
+        tr.addView(getTextView(0, "Net Weight", Color.WHITE, Typeface.BOLD, Color.BLUE));
+        tl.addView(tr, getTblLayoutParams());
+    }
+
+    public void addData() {
+        try {
+            TableLayout tl = findViewById(R.id.table);
+            int i = 0;
+            for (i = 0; i < netWeights.size(); i++) {
+                TableRow tr = new TableRow(this);
+                tr.setLayoutParams(getLayoutParams());
+                tr.addView(getTextView(i, String.valueOf(i+1), Color.WHITE, Typeface.NORMAL, ContextCompat.getColor(this, R.color.colorAccent)));
+                tr.addView(getTextView(i, netBarcodeNos.get(i), Color.WHITE, Typeface.NORMAL, ContextCompat.getColor(this, R.color.colorAccent)));
+                tr.addView(getTextView(i, String.valueOf(netWeights.get(i)), Color.WHITE, Typeface.NORMAL, ContextCompat.getColor(this, R.color.colorAccent)));
+                totalWeight = Math.round((totalWeight + netWeights.get(i)) * 10.0f) / 10.0f;
+                tl.addView(tr, getTblLayoutParams());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
